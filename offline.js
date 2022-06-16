dec7cd292e21e09d531104be2af6d96889ebba91
thitikornthamrongchoochai <107599854+thitikornthamrongchoochai@users.noreply.github.com>
2022-06-16 13:06:27 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1655353185,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/ran-sheet0.png",
+		"images/car-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/star-sheet0.png",
+		"images/bome-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/play-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
